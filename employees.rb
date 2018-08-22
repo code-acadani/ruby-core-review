2f9f11caa3a11884e7f9446ee25421a294262089
dani <dani.zaghian@gmail.com>
2018-08-22 10:53:33 -0700

    represents employees using a class

--- employees.rb
+++ employees.rb
@@ -1,4 +1,4 @@
-# Represent an employee using an array
+# # Represent an employee using an array
 
 # employee1 = ["Majora", "Carter", 80000, true]
 # employee2 = ["Danilo", "Campos", 70000, true]
@@ -6,17 +6,85 @@
 # puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
 
 
-# Represent and employee using a hash
+# # Represent and employee using a hash
 
-# string + hashrocket syntax
+# # string + hashrocket syntax
 # employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
 # employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
 # puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
 
-# symbol + hashrocket syntax
+# # symbol + hashrocket syntax
 # employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
 # puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
 
-# javascript syntax
-employee1 = {first_name: "Majora", last_name: "Carter", salary: 80000, active: true}
-puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
+# # javascript syntax
+# employee1 = {first_name: "Majora", last_name: "Carter", salary: 80000, active: true}
+# puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
+
+
+# Represent an employee using a class instance
+class Employee
+	def initialize(input_first_name, input_last_name, input_salary, input_active)
+		@first_name = input_first_name
+		@last_name = input_last_name
+		@salary = input_salary
+		@active = input_active
+	end
+
+	def print_info
+		puts "#{first_name} #{last_name} makes #{salary} a year."
+	end
+
+	def give_annual_raise
+		@salary = 1.05 * @salary
+	end
+
+	def first_name
+		@first_name
+	end
+
+	def last_name
+		@last_name
+	end
+
+	def salary
+		@salary
+	end
+
+	def active
+		@active
+	end
+
+	def active=(input_active)
+		@active = input_active
+	end
+
+	def last_name=(input_last_name)
+		@last_name = input_last_name
+	end
+	
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1.print_info
+employee2.print_info
+employee1.give_annual_raise
+puts employee1.salary
+puts employee1.first_name
+employee1.active = false
+puts employee1.active
+employee1.last_name = "Campos"
+puts employee1.last_name
+
+
+
+
+
+
+
+
+
+
+
+
